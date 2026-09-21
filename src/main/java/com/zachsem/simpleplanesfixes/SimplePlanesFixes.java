7a737ae36d96116f02939a622de53e93cf1be212
zachsem <4555123+zachsem@users.noreply.github.com>
2026-09-21 14:29:02 -0500

    Align repository source with tested release source

--- src/main/java/com/zachsem/simpleplanesfixes/SimplePlanesFixes.java
+++ src/main/java/com/zachsem/simpleplanesfixes/SimplePlanesFixes.java
@@ -16,7 +16,8 @@ public final class SimplePlanesFixes {
     public static final String NAME = "Simple Planes Fixes";
     public static final String VERSION = "1.1.2";
 
-    public SimplePlanesFixes() {}
+    public SimplePlanesFixes() {
+    }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
